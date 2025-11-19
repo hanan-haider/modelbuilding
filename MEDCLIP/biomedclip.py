f67d39bbdebd93d54a6082f1935672376b281e4d
hanan-haider <137984110+hanan-haider@users.noreply.github.com>
2025-11-19 14:40:41 +0500

    update

--- MEDCLIP/biomedclip.py
+++ MEDCLIP/biomedclip.py
@@ -163,7 +163,7 @@ def create_model(
                 model = torch.jit.script(model)
         else:
             model = load_biomedclip_model(
-                model_name,
+               name = _MODEL_CKPT_PATHS[model_name],
                 precision=precision,
                 device=device,
                 jit=jit,
