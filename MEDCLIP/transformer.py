9c09d6de3c26732c07c2d1efe887dc87ea6a691d
hanan-haider <137984110+hanan-haider@users.noreply.github.com>
2025-11-19 17:58:04 +0500

    update

--- MEDCLIP/transformer.py
+++ MEDCLIP/transformer.py
@@ -6,6 +6,18 @@ from typing import Optional, Tuple, Union
 
 
 
+# From PyTorch internals
+def _ntuple(n):
+    def parse(x):
+        if isinstance(x, collections.abc.Iterable):
+            return x
+        return tuple(repeat(x, n))
+    return parse
+to_2tuple = _ntuple(2)
+
+
+
+
 class TimmModel(nn.Module):
     """
     Timm-based Vision Transformer for BiomedCLIP.
